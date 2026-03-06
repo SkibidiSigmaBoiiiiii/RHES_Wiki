823f9d122b3fc4c44bda7305b1bff4489d688760
Oshibez <flamelighter39@gmail.com>
2026-03-06 20:23:20 +1000

    Comit

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,25 +1,30 @@
-function calculatePPS() {
-    const trashBox = document.getElementById("trashBox").checked
-    const genTemp = document.getElementById("genTemp").checked
-    const turbineTemp = document.getElementById("turbineTemp").checked
-    const hydraulicPump = document.getElementById("hydraulicPump").checked
-    const autoExcitation = document.getElementById("autoExcitation").checked
-    const tailwaterBonus = document.getElementById("tailwaterBonus").checked
-    const demandMet = document.getElementById("demandMet").checked
+// function calculatePPS() {
+//     const trashBox = document.getElementById("trashBox").checked
+//     const genTemp = document.getElementById("genTemp").checked
+//     const turbineTemp = document.getElementById("turbineTemp").checked
+//     const hydraulicPump = document.getElementById("hydraulicPump").checked
+//     const autoExcitation = document.getElementById("autoExcitation").checked
+//     const tailwaterBonus = document.getElementById("tailwaterBonus").checked
+//     const demandMet = document.getElementById("demandMet").checked
 
-    if (!demandMet) {
-        document.getElementById("ppsResult").innerText = "Points per second: 0 (Demand not met)"
-        return
-    }
+//     if (!demandMet) {
+//         document.getElementById("ppsResult").innerText = "Points per second: 0 (Demand not met)"
+//         return
+//     }
 
-    let pps = tailwaterBonus ? 1.38 : 1.20
+//     let pps = tailwaterBonus ? 1.38 : 1.20
 
-    if (autoExcitation) pps *= 0.90
-    if (trashBox) pps *= 0.95
-    if (turbineTemp) pps *= 0.90
-    if (hydraulicPump) pps *= 0.90
-    if (genTemp) pps *= 0.95
+//     if (autoExcitation) pps -= 0.12
+//     if (trashBox) pps -= 0.06
+//     if (turbineTemp) pps -= 0.12
+//     if (hydraulicPump) pps -= 0.12
+//     if (genTemp) pps -= 0.06
 
-    document.getElementById("ppsResult").innerText =
-        "Points per second: " + pps.toFixed(2) + "/s"
-}
+//     if (pps < 0) pps = 0
+
+//     document.getElementById("ppsResult").innerText =
+//         "Points per second: " + pps.toFixed(2) + "/s"
+// }
+
+
+// TEMP REMOVED BECAUSE IM TOO LAZY TO FIX :)
